4a3ce27b1d3f16244e5bbbfc7ff348236321f40a
Conos.io <37426334+conosfera@users.noreply.github.com>
2018-03-16 20:58:27 +0100

    Create email-protector.js

---
+++ email-protector.js
@@ -0,0 +1,9 @@
+//codigo para proteger datos en una web
+//codigo para un correo electronico
+function protector-m()
+//by CONOS.io
+{
+var usuario = "mi-cuenta"
+var dominio = "mi-dominio.com"
+document.write("<a href=\"mailto:" + usuario + "@" + dominio + "\">" + usuario + "@" + dominio + "</a>")
+}
