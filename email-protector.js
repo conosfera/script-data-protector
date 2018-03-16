cc9cc612ae239d2e142c769600529c8f590d82f5
Conos.io <37426334+conosfera@users.noreply.github.com>
2018-03-16 21:17:44 +0100

    Update email-protector.js

--- email-protector.js
+++ email-protector.js
@@ -1,5 +1,4 @@
-//codigo para proteger datos en una web
-//codigo para un correo electronico
+//codigo para proteger un correo electronico
 function protector-m()
 //by CONOS.io
 {
@@ -7,3 +6,15 @@ var usuario = "mi-cuenta"
 var dominio = "mi-dominio.com"
 document.write("<a href=\"mailto:" + usuario + "@" + dominio + "\">" + usuario + "@" + dominio + "</a>")
 }
+
+/*
+1. Cambiar los datos: mi-cuenta y: mi-dominio por los que correspondan
+2. Debes llamar al archivo js que recoje las funciones desde el código de tu página html
+Por ejemplo:
+<script src="js/email-protector.js"></script>
+3. En el lugar que quieras que aparezca el dato, debes incluir la orden que llama a la función específica
+Siguiendo el ejemplo seŕia:
+<script>protector-m()</script>
+Para que el código sea más libiano se debe incluir todas las funciones en un mismo archivo js, nombrando cada función con un nombre diferente
+*/
+
