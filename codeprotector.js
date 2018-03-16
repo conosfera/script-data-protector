374903b2f27f961f112e8a936897495bd87782f4
Conos.io <37426334+conosfera@users.noreply.github.com>
2018-03-16 20:51:55 +0100

    Create codeprotector.js

---
+++ codeprotector.js
@@ -0,0 +1,10 @@
+//codigo para proteger datos en una web
+//codigo para un dato generico
+function protector()
+//by CONOS.io
+{
+var data1 = " dato 1"
+var data2 = " dato 3"
+var data3 = " dato 3"
+document.write( data1 + data2 + data3)
+}
