fb033737704b1d655b0545a61df9127b27ae4b45
Conos.io <37426334+conosfera@users.noreply.github.com>
2018-03-16 21:23:23 +0100

    Update phone-protector.js

--- phone-protector.js
+++ phone-protector.js
@@ -1,4 +1,6 @@
+//Inicio código
 function protector-t()
+//by CONOS.io
 {
 var data1 = "+34 "
 var data2 = "900"
@@ -6,3 +8,17 @@ var data3 = " 900"
 var data4 = " 900"
 document.write( data1 + data2 + data3 + data4)
 }
+//Fin código
+
+/*
+Código javascript para proteger un número de teléfono
+1. Cambiar los datos: +34 y los 900 por los que correspondan
+Maneja los espacios en blanco según tu interés
+2. Debes llamar al archivo js que recoje las funciones desde el código de tu página html
+Por ejemplo:
+<script src="js/phone-protector.js"></script>
+3. En el lugar que quieras que aparezca el dato, debes incluir la orden que llama a la función específica
+Siguiendo el ejemplo seŕia:
+<script>protector-t()</script>
+Para que el código sea más libiano se debe incluir todas las funciones en un mismo archivo js, nombrando cada función con un nombre diferente
+*/
