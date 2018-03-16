efecdf7d9efea95b6b93c7ae0d00bbf02e526528
Conos.io <37426334+conosfera@users.noreply.github.com>
2018-03-16 21:26:28 +0100

    Update codeprotector.js

--- codeprotector.js
+++ codeprotector.js
@@ -1,5 +1,5 @@
-//codigo para proteger datos en una web
-//codigo para un dato generico
+//Inicio código
+
 function protector()
 //by CONOS.io
 {
@@ -8,3 +8,18 @@ var data2 = " dato 3"
 var data3 = " dato 3"
 document.write( data1 + data2 + data3)
 }
+//Fin código
+
+/*
+Código javascript para proteger un dato
+1. Cambiar los datos: dato 1, dato 2 y dato 3 por los que correspondan
+Los espacios en blanco cuentan
+Puedes quitar o añadir datos a la cadena
+2. Debes llamar al archivo js que recoje las funciones desde el código de tu página html
+Por ejemplo:
+<script src="js/codeprotector.js"></script>
+3. En el lugar que quieras que aparezca el dato, debes incluir la orden que llama a la función específica
+Siguiendo el ejemplo seŕia:
+<script>protector()</script>
+Para que el código sea más libiano se debe incluir todas las funciones en un mismo archivo js, nombrando cada función con un nombre diferente
+*/
